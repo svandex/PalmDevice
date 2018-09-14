51fcb60457ab09143faa2022672f6649959c9ad3
XuJiahui <Joyce@XuJiahuideMacBook-Air-2.local>
2018-09-14 15:53:37 +0800

    daily workn 09/14

--- ws/example/dataSend.js
+++ ws/example/dataSend.js
@@ -1,5 +1,13 @@
 function dataSend() {
     //alert('success!');
     var ori = document.getElementsByTagName('input');
+    for (var i=0; i<ori.length; i++) {
+    	// 获得各个文本输入框的字段名和值
+    	var name = ori[i].name;
+    	var value = ori[i].value;
+    	ws.send()
+    }
 
 }
+
+<script type="text/javascript" src="dataSend.js"></script>
